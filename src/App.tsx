ad236a40a313f5e16ab455c8fc65230b8ee19672
unknown <salvatierraprogrammer@gmail.com>
2024-10-23 03:09:26 -0300

    Backend3

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '@layout/Header';
 import Footer from '@layout/Footer';
 import Home from './page/Home';
-import { auth } from '@config/firebase';
+import { auth } from '@config/Firebase';
 import Login from '@auth/Login';
 
 function App() {
